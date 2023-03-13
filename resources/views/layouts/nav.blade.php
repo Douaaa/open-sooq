ed87a9145e1940139070f8653159c0951ddf0f42
fsitpd <appteco.app@hotmail.com>
2023-03-13 20:34:38 +0300

    add

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,1 +1,69 @@
-<h1>Nav</h1>
+<header>
+    <div class="social-nav pt-3">
+            <div class="container">
+                <img src="{{ asset('assets/images/Rectangle_248@2x.png') }}" alt="instagram">
+                <img src="{{ asset('assets/images/Rectangle_247@2x.png') }}" alt="facebook">
+                <img src="{{ asset('assets/images/Rectangle_249@2x.png') }}" alt="tiktok">
+                <img src="{{ asset('assets/images/Rectangle_250@2x.png') }}" alt="whatsapp">
+            </div>
+    </div>
+    <div class="main-header">
+            <nav class="navbar navbar-expand-lg main-nav">
+                <div class="container-fluid">
+                  <a class="navbar-brand" href="#">Logo</a>
+                  
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                      <li class="nav-item">
+                        <a class="navbar-phone d-flex align-items-center" href="tel:0970970975"><i class="fa-solid fa-phone m-1"></i>0970970975</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Project</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Shope</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Video Reels</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Sales Team</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Partners</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Services</a>
+                      </li>
+                      <li class="nav-item post-ads">
+                        <a class="nav-link" href="#">Post Your Ads</a>
+                      </li>
+                      
+                    </ul>
+                    <form class="d-flex m-0" role="search">
+                      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    </form>
+
+                    <li class="nav-item mode">
+                        <a class="nav-link" href="#">Dark Mode</a>
+                    </li>
+                    <li class="nav-item langs">
+                        <a class="nav-link d-flex align-items-center" href="#">
+                            <img src="{{ asset('assets/images/Flag_of_Jordan.svg.png')}}" alt="">
+                            <h3 class="m-1">Ar</h3>
+                        </a>
+                    </li>
+                    <li class="nav-item sign-in">
+                        <a class="nav-link d-flex align-items-center" href="#">
+                            <i class="fa-solid fa-user"></i>
+                            <h3 class="m-1">Sign in</h3>
+                        </a>
+                    </li>
+                  </div>
+                </div>
+            </nav>
+    </div>
+</header>

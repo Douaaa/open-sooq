51dabde2de6b4cd12ad8d5a740eab2688531cc82
fsitpd <appteco.app@hotmail.com>
2023-03-13 22:21:46 +0300

    add

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,24 +1,30 @@
 <header>
     <div class="social-nav pt-3">
-            <div class="container">
-                <img src="{{ asset('assets/images/Rectangle_248@2x.png') }}" alt="instagram">
-                <img src="{{ asset('assets/images/Rectangle_247@2x.png') }}" alt="facebook">
-                <img src="{{ asset('assets/images/Rectangle_249@2x.png') }}" alt="tiktok">
-                <img src="{{ asset('assets/images/Rectangle_250@2x.png') }}" alt="whatsapp">
+            <div class="container-fluid">
+                <div>
+                  <img src="{{ asset('assets/images/Rectangle_248@2x.png') }}" alt="instagram">
+                  <img src="{{ asset('assets/images/Rectangle_247@2x.png') }}" alt="facebook">
+                  <img src="{{ asset('assets/images/Rectangle_249@2x.png') }}" alt="tiktok">
+                  <img src="{{ asset('assets/images/Rectangle_250@2x.png') }}" alt="whatsapp">
+                </div>
+                <a class="navbar-phone" href="tel:0970970975"><i class="fa-solid fa-phone m-1"></i>0970970975</a>
+
             </div>
+
     </div>
     <div class="main-header">
-            <nav class="navbar navbar-expand-lg main-nav">
+            <nav class="navbar navbar-expand-xl main-nav">
                 <div class="container-fluid">
                   <a class="navbar-brand" href="#">Logo</a>
-                  
+                  <a class="navbar-phone" href="tel:0970970975"><i class="fa-solid fa-phone m-1"></i>0970970975</a>
+
                   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                   </button>
                   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+                    <ul class="navbar-nav">
                       <li class="nav-item">
-                        <a class="navbar-phone d-flex align-items-center" href="tel:0970970975"><i class="fa-solid fa-phone m-1"></i>0970970975</a>
                       </li>
                       <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">Project</a>
@@ -38,28 +44,31 @@
                       <li class="nav-item">
                         <a class="nav-link" href="#">Services</a>
                       </li>
-                      <li class="nav-item post-ads">
-                        <a class="nav-link" href="#">Post Your Ads</a>
-                      </li>
-                      
                     </ul>
-                    <form class="d-flex m-0" role="search">
-                      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    </form>
+                    <div class="nav-item post-ads">
+                      <a class="nav-link" href="#">Post Your Ads</a>
+                    </div>
+                    <div class="nav-item search d-flex align-items-center">
+                      <i class="fa-solid fa-search"></i>
+                      <form class="d-flex m-0" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                      </form>
+                    </div>
+
 
                     <li class="nav-item mode">
-                        <a class="nav-link" href="#">Dark Mode</a>
+                        <a class="nav-link p-1" href="#">Dark Mode</a>
                     </li>
                     <li class="nav-item langs">
-                        <a class="nav-link d-flex align-items-center" href="#">
+                        <a class="nav-link d-flex align-items-center p-1" href="#">
                             <img src="{{ asset('assets/images/Flag_of_Jordan.svg.png')}}" alt="">
                             <h3 class="m-1">Ar</h3>
                         </a>
                     </li>
                     <li class="nav-item sign-in">
-                        <a class="nav-link d-flex align-items-center" href="#">
-                            <i class="fa-solid fa-user"></i>
-                            <h3 class="m-1">Sign in</h3>
+                        <a class="nav-link d-flex align-items-center p-1" href="#">
+                          <img src="{{ asset('assets/images/XD_vector_2-29@2x.png')}}" alt="">
+                          <h3 class="m-1">Sign in</h3>
                         </a>
                     </li>
                   </div>

4c6ae069672cb6b21ba513298e14803ed5cf6dfe
Douaaa <douaa.hasan011@gmail.com>
2023-03-13 14:43:15 +0200

    2 commit

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <footer>
     <h1>Footer</h1>
 
-
+the footer
 </footer>
 <script src="{{ asset('assets/js/bootstrap.bundle.min.js')}}"  crossorigin="anonymous"></script>
 <script src="{{ asset('assets/js/jquery.min.js')}}"  crossorigin="anonymous" referrerpolicy="no-referrer"></script>

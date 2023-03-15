4e1066ff1f741f17112e6e7238ed0974a06d0bd8
Douaaa <douaa.hasan011@gmail.com>
2023-03-15 10:44:27 +0200

    new updates on download

--- resources/views/home/download.blade.php
+++ resources/views/home/download.blade.php
@@ -1,1 +1,55 @@
-<h1>Download</h1>
+<link href="{{ asset('assets/css/download.css')}}" rel="stylesheet"  crossorigin="anonymous">
+<section id="download">
+    <div class="download-bg">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="left-section">
+                        <div class="download-title">
+                            <h5>Faster. Easier Free.</h5>
+                            <h3>Download The Application Now</h3>
+                        </div>
+                        <div class="icons-section">
+                            <div class="row">
+                                <div class="col-lg-3 col-md-6 col-sm-12">
+                                    <div class="black-box">
+                                        <img src="{{asset('assets/images/googleplay.png')}}" alt="google-play" title="google-play">
+                                        <a href="">
+
+                                            GET ON  <strong> Google Play</strong>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="col-lg-3 col-md-6 col-sm-12">
+                                    <div class="black-box">
+                                        <img src="{{asset('assets/images/apple.png')}}" alt="apple" title="apple">
+                                        <a href="">
+
+                                            DOWNLOAD ON <strong>Apple Store</strong>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="col-lg-3 col-md-6 col-sm-12">
+                                    <div class="black-box">
+                                        <img src="{{asset('assets/images/huawei.png')}}" alt="huawei" title="huawei">
+                                        <a href="">
+
+                                            EXPLORE IT ON <strong>APPGALLARY</strong>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="col-lg-3 col-md-6 col-sm-12">
+                                    <div class="qr-section">
+                                        <div class="qr-title">Scan QR Code</div>
+                                        <img src="{{asset('assets/images/qr.png')}}" alt="qr-code" title="qr-code">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>

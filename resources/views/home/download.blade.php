b72cb109debe3df7be788dc124152121dcbd6e47
Douaaa <douaa.hasan011@gmail.com>
2023-03-15 13:26:31 +0200

    new updates on download

--- resources/views/home/download.blade.php
+++ resources/views/home/download.blade.php
@@ -6,47 +6,113 @@
                 <div class="col-md-8">
                     <div class="left-section">
                         <div class="download-title">
-                            <h5>Faster. Easier Free.</h5>
+                            <p>Faster. Easier Free.</p>
                             <h3>Download The Application Now</h3>
                         </div>
                         <div class="icons-section">
-                            <div class="row">
-                                <div class="col-lg-3 col-md-6 col-sm-12">
-                                    <div class="black-box">
-                                        <img src="{{asset('assets/images/googleplay.png')}}" alt="google-play" title="google-play">
-                                        <a href="">
-
-                                            GET ON  <strong> Google Play</strong>
-                                        </a>
+                            <div class="top-icons">
+                                <div class="row">
+                                    <div class="col-lg-3 col-md-6 col-sm-12">
+                                        <div class="black-box">
+                                            <img src="{{asset('assets/images/googleplay.png')}}" alt="google-play" title="google-play">
+                                            <a href="">
+                                                GET ON  <strong> Google Play</strong>
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-3 col-md-6 col-sm-12">
+                                        <div class="black-box">
+                                            <img src="{{asset('assets/images/apple.png')}}" alt="apple" title="apple">
+                                            <a href="">
+                                                DOWNLOAD ON <strong>Apple Store</strong>
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-3 col-md-6 col-sm-12">
+                                        <div class="black-box">
+                                            <img src="{{asset('assets/images/huawei.png')}}" alt="huawei" title="huawei">
+                                            <a href="">
+                                                EXPLORE IT ON <strong>APPGALLARY</strong>
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-3 col-md-6 col-sm-12">
+                                        <div class="qr-section">
+                                            <div class="qr-title">Scan QR Code</div>
+                                            <img src="{{asset('assets/images/qr.png')}}" alt="qr-code" title="qr-code">
+                                        </div>
                                     </div>
                                 </div>
-                                <div class="col-lg-3 col-md-6 col-sm-12">
-                                    <div class="black-box">
-                                        <img src="{{asset('assets/images/apple.png')}}" alt="apple" title="apple">
-                                        <a href="">
-
-                                            DOWNLOAD ON <strong>Apple Store</strong>
-                                        </a>
+                            </div>
+                            <div class="bottom-icons">
+                                <div class="row">
+                                    <div class="col-lg-3 col-md-6 col-sm-12">
+                                        <div class="info-icon">
+                                            <div class="d-inline-block img-div">
+                                                <img src="{{asset('assets/images/mail.png')}}" class="info-img">
+                                            </div>
+                                            <div class="d-inline-block">
+                                                <span >Email</span>
+                                                <a href="mailto:Jo@00000000" class="d-block">Jo@00000000</a>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="col-lg-3 col-md-6 col-sm-12">
-                                    <div class="black-box">
-                                        <img src="{{asset('assets/images/huawei.png')}}" alt="huawei" title="huawei">
-                                        <a href="">
-
-                                            EXPLORE IT ON <strong>APPGALLARY</strong>
-                                        </a>
+                                    <div class="col-lg-3 col-md-6 col-sm-12">
+                                        <div class="info-icon">
+                                            <div class="d-inline-block img-div">
+                                                <img src="{{asset('assets/images/phone.png')}}" class="info-img">
+                                            </div>
+                                            <div class="d-inline-block">
+                                                <span >Phone</span>
+                                                <a href="tel:+9627725666666" class="d-block">+9627725666666</a>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="col-lg-3 col-md-6 col-sm-12">
-                                    <div class="qr-section">
-                                        <div class="qr-title">Scan QR Code</div>
-                                        <img src="{{asset('assets/images/qr.png')}}" alt="qr-code" title="qr-code">
+                                    <div class="col-lg-3 col-md-6 col-sm-12">
+                                        <div class="info-icon">
+                                            <div class="d-inline-block img-div">
+                                                <img src="{{asset('assets/images/whatsup.png')}}" class="info-img">
+                                            </div>
+                                            <div class="d-inline-block">
+                                                <span >Whatsapp</span>
+                                                <a href="#" class="d-block">contact Us</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-3 col-md-6 col-sm-12">
+                                        <div class="info-icon">
+                                            <div class="d-inline-block img-div">
+                                                <img src="{{asset('assets/images/person.png')}}" class="info-img">
+                                            </div>
+                                            <div class="d-inline-block">
+                                                <span >Contact Sales</span>
+                                                <a href="#" class="d-block">000000/Sales</a>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+                </div>
+                <div class="col-md-4 position-relative">
+                    <div class="right-section">
+                        <div class="mobile-img">
+                            <img src="{{asset('assets/images/mobile.png')}}">
+                        </div>
+                        <div class="right-bottom-icons">
+                            <span>Follow Us</span>
+                            <ul>
+                                <li><a href="#"><img src="{{asset('assets/images/facebook.png')}}"></a></li>
+                                <li><a href="#"><img src="{{asset('assets/images/twitter.png')}}"></a></li>
+                                <li><a href="#"><img src="{{asset('assets/images/instagram.png')}}"></a></li>
+                                <li><a href="#"><img src="{{asset('assets/images/tiktok.png')}}"></a></li>
+                                <li><a href="#"><img src="{{asset('assets/images/youtube.png')}}"></a></li>
+                            </ul>
+                        </div>
+
+                    </div>
+
                 </div>
             </div>
         </div>

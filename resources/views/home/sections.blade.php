2d20001301e3d2c1ba6b732514278c310da4af9e
fsitpd <appteco.app@hotmail.com>
2023-03-15 22:01:17 +0300

    add

--- resources/views/home/sections.blade.php
+++ resources/views/home/sections.blade.php
@@ -1,1 +1,124 @@
-<h1>Sections</h1>
+<section id="sections" class="py-5">  
+    <div class="container">
+        <div class="top">
+            <p class="col-sm-3 col-md-2 col-lg-1 text-center">All Sections</p>
+            <div class="hr col-sm-6 col-md-8 col-lg-10"></div>
+            <p class="col-sm-3 col-md-2 col-lg-1 text-center">View All</p>
+        </div>
+        <div class="sections d-flex flex-wrap">
+            <div class="section-first d-flex flex-wrap">
+                <div class="section">
+                    <div class="image">
+                        <img src="{{ asset('assets/images/Rectangle_251@2x.png') }}" alt="">
+                    </div>
+                    <div class="title">Car</div>
+                </div>
+                <div class="section">
+                    <div class="image">
+                        <img src="  " alt="">
+                    </div>
+                    <div class="title">Motorcycles</div>
+                </div>
+                <div class="section">
+                    <div class="image">
+                        <img src="  " alt="">
+                    </div>
+                    <div class="title">Mobile & Tablet</div>
+                </div>
+                <div class="section">
+                    <div class="image">
+                        <img src="  " alt="">
+                    </div>
+                    <div class="title">Electronics</div>
+                </div>
+                <div class="section">
+                    <div class="image">
+                        <img src="  " alt="">
+                    </div>
+                    <div class="title">Real estat For sale</div>
+                </div>
+            </div>
+            <div class="section-second d-flex flex-wrap">
+                <div class="section">
+                    <div class="image">
+                    </div>
+                    <div class="title">Real estat For rent</div>
+                </div>
+                <div class="section">
+                    <div class="image">
+                    </div>
+                    <div class="title">pc & video games consoles</div>
+                </div>
+                <div class="section">
+                    <div class="image">
+                    </div>
+                    <div class="title">Pets or Animals</div>
+                </div>
+                <div class="section">
+                    <div class="image">
+                    </div>
+                    <div class="title">Service</div>
+                </div>
+                <div class="section">
+                    <div class="image">
+                    </div>
+                    <div class="title">House Furniture</div>
+                </div>
+            </div>
+            <div class="section-third d-flex flex-wrap">
+                <div class="section">
+                    <div class="image">
+                    </div>
+                    <div class="title">Toys</div>
+                </div>
+                <div class="section">
+                    <div class="image">
+                    </div>
+                    <div class="title">Women Fashion</div>
+                </div>
+                <div class="section">
+                    <div class="image">
+                    </div>
+                    <div class="title">Man Fashion</div>
+                </div>
+                <div class="section">
+                    <div class="image">
+                    </div>
+                    <div class="title">Sporte</div>
+                </div>
+                <div class="section">
+                    <div class="image">
+                    </div>
+                    <div class="title">Platform Education</div>
+                </div>
+            </div>
+            <div class="section-fourth d-flex flex-wrap">
+                <div class="section">
+                    <div class="image">
+                    </div>
+                    <div class="title">Shop</div>
+                </div>
+                <div class="section">
+                    <div class="image">
+                    </div>
+                    <div class="title">Kids</div>
+                </div>
+                <div class="section">
+                    <div class="image">
+                    </div>
+                    <div class="title">Book</div>
+                </div>
+                <div class="section">
+                    <div class="image">
+                    </div>
+                    <div class="title">Food</div>
+                </div>
+                <div class="section">
+                    <div class="image">
+                    </div>
+                    <div class="title">Jobs</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>

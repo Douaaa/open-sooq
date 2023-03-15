e6694efa8a2ee39e9c1249f41e9fd4c81b245115
Douaaa <douaa.hasan011@gmail.com>
2023-03-15 21:55:24 +0200

    platforms add

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,13 +11,8 @@
     <link href="{{ asset('assets/css/bootstrap.min.css')}}" rel="stylesheet"  crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     {{-- swipper --}}
-    <link
-    rel="stylesheet"
-    href="https://unpkg.com/swiper@8/swiper-bundle.min.css"
-  />
-  
-  <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
-    {{-- swipper --}}
+    <link rel="stylesheet"  href="{{asset('assets/css/swiper-bundle.min.css')}}" />
+    <script src="{{asset('assets/js/swiper-bundle.min.js')}}"></script>
     <!-- Template Main CSS File -->
     @if(trans('all.dir') != 'rtl')
         <link href="{{ asset('assets/css/style.css')}}" rel="stylesheet">

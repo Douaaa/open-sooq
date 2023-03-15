2158a8c40be55e788bfe463aa29df37dfc704ed1
fsitpd <appteco.app@hotmail.com>
2023-03-15 15:31:39 +0300

    add

--- resources/views/home/status.blade.php
+++ resources/views/home/status.blade.php
@@ -4,32 +4,101 @@
         <!-- Additional required wrapper -->
         <div class="swiper-wrapper">
         <!-- Slides -->
-        <div class="swiper-slide">
-            <img src="{{ asset('assets/images/Flag_of_Jordan.svg.png') }}" alt="">
+        <div class="swiper-slide statu">
+            <div class="image">
+                <img src="{{ asset('assets/images/Flag_of_Jordan.svg.png') }}" alt="">
+            </div>
+            <p>Persom Name</p>
         </div>
-        <div class="swiper-slide">
-            <img src="{{ asset('assets/images/Flag_of_Jordan.svg.png') }}" alt="">
+        <div class="swiper-slide statu">
+            <div class="image">
+                <img src="{{ asset('assets/images/Flag_of_Jordan.svg.png') }}" alt="">
+            </div>
+            <p>Persom Name</p>
         </div>
-        <div class="swiper-slide">
-            <img src="{{ asset('assets/images/Flag_of_Jordan.svg.png') }}" alt="">
+        <div class="swiper-slide statu">
+            <div class="image">
+                <img src="{{ asset('assets/images/Flag_of_Jordan.svg.png') }}" alt="">
+            </div>
+            <p>Persom Name</p>
         </div>
-        <div class="swiper-slide">
-            <img src="{{ asset('assets/images/Flag_of_Jordan.svg.png') }}" alt="">
+        <div class="swiper-slide statu">
+            <div class="image">
+                <img src="{{ asset('assets/images/Flag_of_Jordan.svg.png') }}" alt="">
+            </div>
+            <p>Persom Name</p>
         </div>
-        <div class="swiper-slide">
-            <img src="{{ asset('assets/images/Flag_of_Jordan.svg.png') }}" alt="">
+        <div class="swiper-slide statu">
+            <div class="image">
+                <img src="{{ asset('assets/images/Flag_of_Jordan.svg.png') }}" alt="">
+            </div>
+            <p>Persom Name</p>
         </div>
-        <div class="swiper-slide">
-            <img src="{{ asset('assets/images/Flag_of_Jordan.svg.png') }}" alt="">
+        <div class="swiper-slide statu">
+            <div class="image">
+                <img src="{{ asset('assets/images/Flag_of_Jordan.svg.png') }}" alt="">
+            </div>
+            <p>Persom Name</p>
         </div>
-        <div class="swiper-slide">
-            <img src="{{ asset('assets/images/Flag_of_Jordan.svg.png') }}" alt="">
+        <div class="swiper-slide statu">
+            <div class="image">
+                <img src="{{ asset('assets/images/Flag_of_Jordan.svg.png') }}" alt="">
+            </div>
+            <p>Persom Name</p>
         </div>
-        <div class="swiper-slide">
-            <img src="{{ asset('assets/images/Flag_of_Jordan.svg.png') }}" alt="">
+        <div class="swiper-slide statu">
+            <div class="image">
+                <img src="{{ asset('assets/images/Flag_of_Jordan.svg.png') }}" alt="">
+            </div>
+            <p>Persom Name</p>
         </div>
-        <div class="swiper-slide">
-            <img src="{{ asset('assets/images/Flag_of_Jordan.svg.png') }}" alt="">
+        <div class="swiper-slide statu">
+            <div class="image">
+                <img src="{{ asset('assets/images/Flag_of_Jordan.svg.png') }}" alt="">
+            </div>
+            <p>Persom Name</p>
+        </div>
+        <div class="swiper-slide statu">
+            <div class="image">
+                <img src="{{ asset('assets/images/Flag_of_Jordan.svg.png') }}" alt="">
+            </div>
+            <p>Persom Name</p>
+        </div>
+        <div class="swiper-slide statu">
+            <div class="image">
+                <img src="{{ asset('assets/images/Flag_of_Jordan.svg.png') }}" alt="">
+            </div>
+            <p>Persom Name</p>
+        </div>
+        <div class="swiper-slide statu">
+            <div class="image">
+                <img src="{{ asset('assets/images/Flag_of_Jordan.svg.png') }}" alt="">
+            </div>
+            <p>Persom Name</p>
+        </div>
+        <div class="swiper-slide statu">
+            <div class="image">
+                <img src="{{ asset('assets/images/Flag_of_Jordan.svg.png') }}" alt="">
+            </div>
+            <p>Persom Name</p>
+        </div>
+        <div class="swiper-slide statu">
+            <div class="image">
+                <img src="{{ asset('assets/images/Flag_of_Jordan.svg.png') }}" alt="">
+            </div>
+            <p>Persom Name</p>
+        </div>
+        <div class="swiper-slide statu">
+            <div class="image">
+                <img src="{{ asset('assets/images/Flag_of_Jordan.svg.png') }}" alt="">
+            </div>
+            <p>Persom Name</p>
+        </div>
+        <div class="swiper-slide statu">
+            <div class="image">
+                <img src="{{ asset('assets/images/Flag_of_Jordan.svg.png') }}" alt="">
+            </div>
+            <p>Persom Name</p>
         </div>
 
         </div>
@@ -42,30 +111,33 @@
 
 <script>
     const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  loop: true,
-  centeredSlides: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  breakpoints: {
-    // when window width is >= 320px
-    768: {
-      slidesPerView: 3,
-      spaceBetween: 30
+    // Optional parameters
+    loop: true,
+    autoplay: {
+        delay: 5000,
     },
-    // when window width is >= 480px
-    1024: {
-      slidesPerView: 4,
-      spaceBetween: 40
+    centeredSlides: false,
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
-    // when window width is >= 640px
-    1280: {
-      slidesPerView: 5,
-      spaceBetween: 50
+    breakpoints: {
+        // when window width is >= 320px
+        768: {
+        slidesPerView: 5,
+        spaceBetween: 15
+        },
+        // when window width is >= 480px
+        1024: {
+        slidesPerView: 9,
+        spaceBetween: 15
+        },
+        // when window width is >= 640px
+        1280: {
+        slidesPerView: 12,
+        spaceBetween: 15
+        }
     }
-  }
-});
+    });
 </script>
